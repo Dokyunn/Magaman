41bb3332840103b89e3a9b7e226fcc5818b71ac9
Dokyunn <45741804+Dokyunn@users.noreply.github.com>
2018-12-09 22:44:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,15 @@
-const discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", async () => {
-  console.log(`${client.user.username} is online!`);
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  
-if(cmd === `${prefix}hello`) {
-  return message.channel.send("Hello!");
-}
-  
-bot.logn(process.env.BOT_TOKEN)
-
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
